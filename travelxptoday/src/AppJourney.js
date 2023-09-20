b4fb8e38e65ce5d7866335f907a3a4ede5cc0ce7
Jelle07 <113422379+JManders07@users.noreply.github.com>
2023-09-20 13:22:59 +0200

    Fixed display data problem
    Added div around mapping the journeydata. Also added a key

--- travelxptoday/src/AppJourney.js
+++ travelxptoday/src/AppJourney.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import JourneyCard from './Components/Journeycard';
 
 const App = () => {
   const [journeyData, setJourneyData] = useState(null);
@@ -29,16 +28,20 @@ const App = () => {
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <div className="journey-card p-6 bg-white rounded-lg shadow-md">
-      <h2 className="text-xl font-bold mb-4">Journey from {journeyData.start_location} to {journeyData.end_location}</h2>
-      <p className="mb-2"><strong>Start Time:</strong> {journeyData.start_time}</p>
-      <p className="mb-2"><strong>End Time:</strong> {journeyData.end_time}</p>
-      <p className="mb-2"><strong>Duration:</strong> {journeyData.duration}</p>
-      <p className="mb-2"><strong>Mode of Transport:</strong> {journeyData.mode_of_transport}</p>
-      <p className="mb-2"><strong>Status:</strong> {journeyData.status}</p>
-      <p className="mb-4"><strong>Notes:</strong> {journeyData.notes}</p>
-      <h3 className="text-lg font-medium mb-2">Participants:</h3>
-      
+    <div>
+      {journeyData.map((journey) => (
+        <div className="journey-card p-6 bg-white rounded-lg shadow-md" key={journey.id}>
+          <h2 className="text-xl font-bold mb-4">Journey from {journey.start_location} to {journey.end_location}</h2>
+          <p className="mb-2"><strong>Start Time:</strong> {journey.start_time}</p>
+          <p className="mb-2"><strong>End Time:</strong> {journey.end_time}</p>
+          <p className="mb-2"><strong>Duration:</strong> {journey.duration}</p>
+          <p className="mb-2"><strong>Mode of Transport:</strong> {journey.mode_of_transport}</p>
+          <p className="mb-2"><strong>Status:</strong> {journey.status}</p>
+          <p className="mb-4"><strong>Notes:</strong> {journey.notes}</p>
+          <h3 className="text-lg font-medium mb-2">Participants:</h3>
+          {/* Add code here to display participants */}
+        </div>
+      ))}
     </div>
   );
 }
