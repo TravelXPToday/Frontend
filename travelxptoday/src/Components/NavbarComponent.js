cd4a1a9db642489d8ddd5b924d54958936c9cb1d
artofghost <67277022+Artnooijen@users.noreply.github.com>
2023-10-04 11:59:42 +0200

    test

--- travelxptoday/src/Components/NavbarComponent.js
+++ travelxptoday/src/Components/NavbarComponent.js
@@ -1,17 +1,24 @@
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import ImageLogo from '../Assets/Image/Logo.jfif';
+import { Cursor, useTypewriter } from "react-simple-typewriter";
 const NavBar = () => {
     const [isOpen, setIsOpen] = useState(false);
     const handleLinkClick = () => {
         setIsOpen(false);  // This will close the navbar when a link is clicked
     };
+    const [text] = useTypewriter({
+        words: ["TravelXPToday", "Where are you going today?","TravelXPToday", "Adventure!!","TravelXPToday","“To Travel is to Live.”"],
+        loop: true,
+        delaySpeed: 2000,
+      });
     return (
         <nav className="py-4 shadow-md border-b-4 bg-slate-900 border-teal-600 sticky top-0 z-20">
                 <div className="container mx-auto flex justify-between items-center">
                 <div className='flex justify-between items-center'>          
                     <img src={ImageLogo} alt="logo" className="h-16 w-16 rounded-full object-fill" /> 
-                    <a href="/" className="text-2xl font-semibold text-pink-500 hover:border-b-2 hover:border-pink-500">TravelXPToday</a>
+                    <a href="/" className="ml-4 text-2xl font-semibold text-pink-500 hover:border-b-2 hover:border-pink-500">{text}</a>
+                    <Cursor className="font-bold" cursorColor="#e91e63" />
                 </div>
                 <ul className="md:flex hidden space-x-4 mr- ">
                     <li><Link to="/journey" className="text-teal-200 focus:text-white focus:rounded-full focus:bg-pink-500 hover:bg-pink-500 hover:text-white text-s font-semibold py-2 px-6 rounded-full transition duration-300 ease-in-out hover:drop-shadow-lg">Journeys</Link></li>
--- travelxptoday/src/Components/TravelerComponent.js
+++ travelxptoday/src/Components/TravelerComponent.js
@@ -1,7 +1,58 @@
 import React, { useEffect, useState } from "react";
+import { useRef } from "react";
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform
+} from "framer-motion";
+import 'tailwindcss/tailwind.css';
+
+function useParallax(value, distance) {
+  return useTransform(value, [0, 1], [-distance, distance]);
+}
+
+const Image = ({ id, imageUrl, text }) => {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref });
+  const y = useParallax(scrollYProgress, 300);
+
+  return (
+    <section ref={ref} className="snap-start relative h-screen flex items-center justify-center">
+      <img src={imageUrl} alt="Custom" className="w-full h-full object-cover"/>
+      <motion.h2 style={{ y }} className="z-10 text-4xl text-white absolute">{text}</motion.h2>
+    </section>
+  );
+}
 
 const TravelerComponent = () => {
-return(<div>test</div>)
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
 
+  });
+
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const fetchedImages = [
+      {id: 1, url: 'https://images.unsplash.com/photo-1695753579372-498a4a382ec9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80', text: 'Custom Text 1'},
+      {id: 2, url: 'https://images.unsplash.com/photo-1695638327343-f204623e1f85?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1335&q=80', text: 'Custom Text 2'},
+    ];
+
+    setImages(fetchedImages);
+  }, []); 
+
+  return (
+    <div className="relative snap snap-y-mandatory">
+      {images.map((image) => (
+        <Image id={image.id} imageUrl={image.url} text={image.text} key={image.id} />
+      ))}
+      <motion.div className="fixed top-24 left-0 w-full bg-pink-500 z-50" style={{ height: '5px', scaleX, originX: 0 }} />
+    </div>
+  );
 }
+
 export default TravelerComponent;
