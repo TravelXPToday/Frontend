2e23bfcdee9551db22f0238730bee02b2117c069
Jelle07 <113422379+JManders07@users.noreply.github.com>
2023-10-11 13:38:48 +0200

    Cleanup warnings

--- travelxptoday/src/Components/AllJourneysComponent.js
+++ travelxptoday/src/Components/AllJourneysComponent.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ModalJourneyComponent from "./ModalJourneyComponent";
 import { Link } from "react-router-dom";
-import { motion, useScroll } from "framer-motion"
 import ScrollComponent from "./ScrollComponent";
 
 const AllJourneyComponents = () => {
